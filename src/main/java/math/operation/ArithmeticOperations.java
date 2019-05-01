f4002876416b44e0e4acba90fb9435cd5ad6f75d
QUINI <bluequini23@gmail.com>
2019-05-01 13:46:48 -0500

    Multiplication added

--- src/main/java/math/operation/ArithmeticOperations.java
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -3,11 +3,14 @@ package math.operation;
 public class ArithmeticOperations {
 
 	public Integer add(Integer a, Integer b){
-
 		return a+b;
 	}
 	
 	public Integer substrac(Integer a, Integer b) {
 		return a-b;
+	}
+	
+	public Integer multiplication(Integer a, Integer b) {
+		return a*b;
 	}
 }
