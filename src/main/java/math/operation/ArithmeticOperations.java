cbe166b09bff5a37e7e007863d0f5b5861be0134
QUINI <bluequini23@gmail.com>
2019-05-01 12:51:33 -0500

    Upload Files Commit

---
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -0,0 +1,12 @@
+package math.operation;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b){
+		return a+b;
+	}
+	
+	public Integer substrac(Integer a, Integer b) {
+		return a-b;
+	}
+}
